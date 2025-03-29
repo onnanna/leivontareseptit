105e347bed8e123da74d3085c46d7989a80d5cc8
Häkkinen Anna K O <hakkanna@cubbli24-gold.cs.helsinki.fi>
2025-03-29 22:03:05 +0200

    Add edit feature

--- app.py
+++ app.py
@@ -32,8 +32,24 @@ def create_item():
 
     items.add_item(title, description, user_id)
 
-
     return redirect("/")
+
+@app.route("/edit_item/<int:item_id>")
+def edit_item(item_id):
+    item = items.get_item(item_id)
+    return render_template("edit_item.html", item=item)
+
+
+@app.route("/update_item", methods=["POST"])
+def update_item():
+    item_id = request.form["item_id"]
+    title = request.form["title"]
+    description = request.form["description"]
+
+    items.update_item(item_id, title, description)
+
+    return redirect("/item/" + str(item_id))
+
 
 @app.route("/register")
 def register():
--- items.py
+++ items.py
@@ -12,11 +12,19 @@ def get_items():
 
 
 def get_item(item_id):
-    sql = """SELECT items.title,
+    sql = """SELECT items.id,
+                    items.title,
                     items.description,
+                    users.id user_id,
                     users.username
             FROM items, users
             WHERE items.user_id = users.id AND
                   items.id = ?"""
     return db.query(sql, [item_id])[0]
 
+
+def update_item(item_id, title, description):
+    sql = """UPDATE items SET title=?,
+                              description = ?
+                          WHERE id = ?"""
+    db.execute(sql, [title, description, item_id])
