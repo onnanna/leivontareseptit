978231f02e8fb61d789cc89e31660460367792ef
Häkkinen Anna K O <hakkanna@cubbli24-gold.cs.helsinki.fi>
2025-05-02 22:04:14 +0300

    Fix item removal (again)

--- items.py
+++ items.py
@@ -99,9 +99,12 @@ def update_item(item_id, title, description, classes):
 
 def remove_item(item_id):
 
+    sql = "DELETE FROM comments WHERE item_id = ?"
+    db.execute(sql, [item_id])
+    sql = "DELETE FROM images WHERE item_id = ?"
+    db.execute(sql, [item_id])
     sql = "DELETE FROM item_classes WHERE item_id = ?"
     db.execute(sql, [item_id])
-
     sql = "DELETE FROM items WHERE id = ?"
     db.execute(sql, [item_id])
 
