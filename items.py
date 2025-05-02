54ae07ca872658f57c8136bc04ba8d83a7239798
Häkkinen Anna K O <hakkanna@cubbli24-gold.cs.helsinki.fi>
2025-05-02 21:32:45 +0300

    Use CSS and improve item list

--- items.py
+++ items.py
@@ -65,8 +65,10 @@ def get_classes(item_id):
 
 
 def get_items():
-    sql = "SELECT id, title FROM items ORDER BY id DESC"
-
+    sql = """SELECT items.id, items.title, users.id user_id, users.username
+            FROM items, users
+            WHERE items.user_id = users.id
+            ORDER BY items.id DESC"""
     return db.query(sql)
 
 
