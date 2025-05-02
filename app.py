6476d3afed17e5b58bf07bb564c69fafbac1af5d
Häkkinen Anna K O <hakkanna@cubbli24-gold.cs.helsinki.fi>
2025-05-02 21:38:21 +0300

    Go directly to new item

--- app.py
+++ app.py
@@ -106,8 +106,9 @@ def create_item():
             classes.append((class_title, class_value))
 
     items.add_item(title, description, user_id, classes)
+    item_id = db.last_insert_id()
 
-    return redirect("/")
+    return redirect("/item/" + str(item_id))
 
 @app.route("/edit_item/<int:item_id>")
 def edit_item(item_id):
